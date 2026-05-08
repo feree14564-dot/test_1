f42efbf839387ad2f2896c6add4ee84a0714dba1
feree14564-dot <feree14564@gmail.com>
2026-05-08 19:14:33 +0700

    kol-vo reber

--- laba_14/yprazhneniya.cpp
+++ laba_14/yprazhneniya.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <io.h>
+#include <fcntl.h>
+#include <set>
 
 using namespace std;
 
 int main()
 {
+    // _setmode(_fileno(stdout), _O_U16TEXT);  
 	ifstream fin("input.txt");
     if (!fin.is_open()) {
         cerr << "Error, file did not oppend: input.txt" << endl;
         return 1; // завершаем программу с ошибкой
     }
-    else cout << "hello world";
-
-    // Позже сюда добавим чтение рёбер
-
+    int u, v;
+    vector<pair<int, int>> edges;
+    set<int> vertices;
+    while (fin >> u >> v) {
+    cout << "Edge: " << u << " - " << v << endl;
+    edges.push_back({u, v}); // Добавить ребро (u, v)
+    vertices.insert(u);      // Добавить вершину u (если уже есть — игнорирует)
+    vertices.insert(v);      // Добавить вершину v
+    }
+    cout << "Total vertices: " << vertices.size() << endl;
     fin.close(); // можно и без этого (деструктор закроет), но явно — лучше
-    return 0;
+
 }

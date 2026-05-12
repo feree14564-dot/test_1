0a5ac2cf649819c2d424cfe8f7ba71a35d8ee8b4
feree14564-dot <feree14564@gmail.com>
2026-05-12 23:07:57 +0700

    yprazhnenie 1

--- laba_14/yprazhneniya.cpp
+++ laba_14/yprazhneniya.cpp
@@ -4,11 +4,15 @@
 #include <io.h>
 #include <fcntl.h>
 #include <set>
+#include <map>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    map<int, int> vertexToIndex;
+    int index = 0;
     // _setmode(_fileno(stdout), _O_U16TEXT);  
 	ifstream fin("input.txt");
     if (!fin.is_open()) {
@@ -23,8 +27,39 @@ int main()
     edges.push_back({u, v}); // Добавить ребро (u, v)
     vertices.insert(u);      // Добавить вершину u (если уже есть — игнорирует)
     vertices.insert(v);      // Добавить вершину v
+    
+    for (int v : vertices) vertexToIndex[v] = index++;
     }
+
+    vector<int> vertexList(vertices.begin(), vertices.end());
+
+    for (int i = 0; i< vertexList.size(); i++){
+        vertexToIndex[vertexList[i]] = i;
+    }
+
+    //создаем матрицу смежности
+    int n = vertexList.size();
+    vector<vector<int>> matrix(n, vector<int>(n,0));
+
+    // Заполнение Матрицы
+    for (auto edge :edges) {
+        int u = edge.first;
+        int v = edge.second;
+        int i = vertexToIndex[u];
+        int j = vertexToIndex[v];
+        matrix[i][j] = 1;
+        matrix[j][i] = 1;   
+    }
+
     cout << "Total vertices: " << vertices.size() << endl;
-    fin.close(); // можно и без этого (деструктор закроет), но явно — лучше
+
+    ofstream fout("matrix.txt");
+for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+        fout << matrix[i][j] << " ";
+    }
+    fout << "\n";
+}
+    fin.close(); 
 
 }
